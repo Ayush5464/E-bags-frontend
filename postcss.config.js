b87d61c04ce4d2d41fb8e9ef708fbb8b385b9a31
ayush gupta <ag298744@gmail.com>
2025-09-10 13:00:24 +0530

    new run command

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 // postcss.config.js
 export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
+    plugins: [
+        require('@tailwindcss/postcss'),
+        require('autoprefixer'),
+    ],
 }
