36891bbba8159d2ddc7c515bd2645d987723fb51
ayush gupta <ag298744@gmail.com>
2025-09-10 14:38:47 +0530

    new change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwind from "@tailwindcss/vite"; // ✅ only if installed
 
 export default defineConfig({
-  plugins: [react(), tailwind()],
+  plugins: [react()],
+  server: {
+    port: 5173
+  }
 });
