813bff794d6bda3806dbf36e3c58247c0a48e3ac
ayush gupta <ag298744@gmail.com>
2025-09-10 16:32:24 +0530

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
   },
+  darkMode: 'media', // optional
   plugins: [],
-}
+};
+  
