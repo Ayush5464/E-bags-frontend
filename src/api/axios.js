456fa719ef44310cfc69ce8ad6955bf04ca96eec
ayush gupta <ag298744@gmail.com>
2025-09-10 21:18:57 +0530

    new

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://e-bags-backend.onrender.com/ebagmart",
-  withCredentials: true, // needed for cookies
+  withCredentials: true, // MUST be true to send cookies!
 });
 
 export default API;
