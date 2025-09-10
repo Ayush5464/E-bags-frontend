6ac33ec0600db079d7c59cf35017baf8611969b5
ayush gupta <ag298744@gmail.com>
2025-09-10 20:33:20 +0530

    new

--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { useAuthStore } from "../store/useAuthStore";
+import { Loader2 } from "lucide-react";
 
 function ProtectedRoutes({ children, adminOnly = false }) {
   const { user, loading } = useAuthStore(); // add loading state from store
@@ -10,7 +11,9 @@ function ProtectedRoutes({ children, adminOnly = false }) {
   if (loading) {
     return (
       <div className="flex items-center justify-center h-screen bg-gray-800">
-        <div className="animate-spin text-gray-500 text-4xl">⏳</div>
+        <div className="animate-spin text-gray-500 text-4xl">
+          <Loader2 className="animate-spin" size={40} />
+        </div>
       </div>
     );
   }
